42c2cfb59809f822f86f482f3290ef3414731d03
Daniel Schroeder <danielschroederjr@gmail.com>
2017-04-04 08:52:01 -0400

    added subtraction table

--- MathFacts/Program.cs
+++ MathFacts/Program.cs
@@ -15,6 +15,7 @@ namespace MathFacts
             int choice = 0;
             Addition addTable = new Addition();
             Multiplication timesTable = new Multiplication();
+            Subtraction minusTable = new Subtraction();
             do
             {
                 MainAppTitle();
@@ -144,9 +145,71 @@ namespace MathFacts
 
                     } while (continueChoice == true);
                 }
-                
-                
-            } while (choice != 3);
+                if (choice == 3)
+                {
+                    bool continueChoice = true;
+                    int startNum = 0;
+                    int endNum = 0;
+                    //TODO = title
+                    do
+                    {
+
+                        minusTable.SubtractionTitle();
+                        try
+                        {
+                            Console.WriteLine("Enter your starting number");
+                            startNum = Int32.Parse(Console.ReadLine());
+                            Console.WriteLine("Enter your ending number");
+                            endNum = Int32.Parse(Console.ReadLine());
+                        }
+                        catch (FormatException)
+                        {
+                            Console.WriteLine("Please enter a valid number");
+                            Console.ReadLine();
+
+                        }
+                        finally
+                        {
+                            //TODO = validate startNum and endNum
+                            if (startNum > 0 && endNum <= 10)
+                            {
+                                minusTable.SubtractionTitle();
+                                Console.WriteLine("");
+                                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                                Console.WriteLine("Your addition Table for {0} - {1}", startNum, endNum);
+                                Console.ForegroundColor = ConsoleColor.White;
+                                Console.WriteLine("");
+                                minusTable.SubtractionTitle(startNum, endNum);
+
+                                Console.WriteLine("");
+                                try
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Green;
+                                    Console.WriteLine("Would you like more addition facts [y/n]");
+                                    Console.ForegroundColor = ConsoleColor.White;
+                                    string moreFacts = Console.ReadLine();
+                                    if (moreFacts == "y")
+                                    {
+                                        continueChoice = true;
+                                    }
+                                    else if (moreFacts == "n")
+                                    {
+                                        continueChoice = false;
+                                    }
+                                }
+                                catch (Exception)
+                                {
+
+                                    throw;
+                                }
+                            }
+
+                        }
+                    } while (continueChoice == true);
+
+                }
+
+            } while (choice != 4);
 
 
         }
@@ -161,7 +224,8 @@ namespace MathFacts
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("Option 1: Addition Facts");
             Console.WriteLine("Option 2: Multiplication Facts");
-            Console.WriteLine("Option 3: Leave Math Facts");
+            Console.WriteLine("Option 3: Subtraction Facts");
+            Console.WriteLine("Option 4: Leave Math Facts");
             //TODO = handle wrong input, fix the exception handling
             choice = Int32.Parse(Console.ReadLine());
             return choice;
---
+++ MathFacts/Subtraction.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathFacts
+{
+    class Subtraction
+    {
+        public void SubtractionTitle()
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Red;
+            string appTitle = @"  _________    ___.    __                        __  .__                ___________     ___.   .__          
+ /   _____/__ _\_ |___/  |_____________    _____/  |_|__| ____   ____   \__    ___/____ \_ |__ |  |   ____  
+ \_____  \|  |  \ __ \   __\_  __ \__  \ _/ ___\   __\  |/  _ \ /    \    |    |  \__  \ | __ \|  | _/ __ \ 
+ /        \  |  / \_\ \  |  |  | \// __ \\  \___|  | |  (  <_> )   |  \   |    |   / __ \| \_\ \  |_\  ___/ 
+/_______  /____/|___  /__|  |__|  (____  /\___  >__| |__|\____/|___|  /   |____|  (____  /___  /____/\___  >
+        \/          \/                 \/     \/                    \/                 \/    \/          \/ ";
+
+            Console.WriteLine(appTitle);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("You will be able to select a starting number and an ending Number between 1 and 10");
+            Console.WriteLine("Next you will see the bumbers between these selections be subtracted");
+            Console.WriteLine("to numbers 1 - 10");
+            Console.WriteLine("");
+            Console.WriteLine("Select a starting number between 1 and 10");
+        }
+
+        public void SubtractionTitle(int startNum, int endNum)
+        {
+            //TODO = build out the logic of the addition table
+            for (int i = startNum  - 1; i <= endNum; i--)
+            {
+                if (i == startNum - 1)
+                {
+                    Console.Write(String.Format("{0, 6}", "n"));
+                }
+                else
+                {
+                    Console.Write(String.Format("{0,6}", i));
+                }
+
+            }
+
+            Console.WriteLine("\n");
+
+            //keeps the loop running
+            for (int i = 10; i <= 1; i--)
+            {
+                Console.Write(String.Format("{0, 6}", i));
+                for (int b = startNum; b <= endNum; b--)
+                {
+                    string output = String.Format("{0, 6}", i - b);
+                    Console.Write(output);
+                }
+                Console.WriteLine("");
+            }
+
+
+        }
+    }
+}
